798c274036f0237ac0e5997e7523f6640b1c6d4a
CVSCharan <charan.cvs@gmail.com>
2022-08-06 01:47:18 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import LoginForm from "./components/LoginForm";
 import Home from "./components/Home";
@@ -10,13 +10,13 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import "./App.css";
 
 const App = () => (
-  <Routes>
-    <Route exact path="/login" element={<LoginForm />} />
-    <ProtectedRoute exact path="/" element={<Home />} />
-    <ProtectedRoute exact path="/products" element={<Products />} />
-    <ProtectedRoute exact path="/cart" element={<Cart />} />
-    <Route element={<NotFound />} />
-  </Routes>
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
+    <Route component={NotFound} />
+  </Switch>
 );
 
 export default App;
--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,11 +1,11 @@
-import { Route, Navigate } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Cookies from "js-cookie";
 
 const ProtectedRoute = (props) => {
   const token = Cookies.get("jwt_token");
 
   if (token === undefined) {
-    return <Navigate to="/login" />;
+    return <Redirect to="/login" />;
   }
 
   return <Route {...props} />;
